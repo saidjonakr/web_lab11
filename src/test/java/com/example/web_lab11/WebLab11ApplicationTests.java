36cf301ab5aaa952bfbae80afbf48f12ac1dd40f
saidjon <s.akramjonov@newuu.uz>
2025-03-28 17:24:37 +0500

    new

---
+++ src/test/java/com/example/web_lab11/WebLab11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_lab11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebLab11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
